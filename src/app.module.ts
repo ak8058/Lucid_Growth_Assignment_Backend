7dc221748bfa6207d8f896820283bfcc2781a29e
Anil Kumar <anil19062004@gmail.com>
2025-09-03 20:08:19 +0530

    Connect To Mongodb Altas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ImapModule } from './imap/imap.module';
-import { EmailModule } from './email/email.module';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ImapModule } from "./imap/imap.module";
+import { EmailModule } from "./email/email.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
-   ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb+srv://GvHxB1xpJExwLT6a@cluster0.avtytow.mongodb.net/'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ||
+        "mongodb+srv://GvBa@cluster0.avtytow.mongodb.net/"
+    ),
     ImapModule,
     EmailModule,
   ],
 })
 export class AppModule {}
-
-
